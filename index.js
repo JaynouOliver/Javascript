a425d284c65414176ccf89fc67ae62c4ceba8234
Subhrakamal Das <jyb2084.usa@gmail.com>
2021-12-10 17:34:30 +0530

    Javascript

---
+++ index.js
@@ -0,0 +1,180 @@
+// // var number = 32;
+// // var number2 = 43;
+// // console.log(number + number2);
+
+// // // string;
+
+// // var stings = "thisii s terkfnwkfw";
+
+
+// // // objects;
+
+// // var marks = {
+// //      ravi: 43,
+// //      subham: 89,
+// //      kali: 90.4498759348,
+// // };
+
+// // console.log(marks);
+
+// // // booleans;
+
+// // var a = true;
+// // var b = false;
+
+// // // undefined;
+
+// // var und = undefined;
+// // console.log(und);
+// // var und;
+
+// // // null
+// // var n = null;
+
+// // // 'at  a veery highlevel therw are tqo type so f data type sin javascript type';
+// // //  1 .primitive data types;
+// // //  undefined, null, null, null, number, string, boolean;
+
+// // // 2. reference data types;
+// // // array, objects;
+
+// // //  arrays;
+
+// // var array = [3, 4, 5, 6, 7, "bablu", 8, 9,20];
+// // console.log(array);
+
+
+// // // 
+
+
+// // operators;
+
+// var a = 34;
+// var b = 56;
+
+// console.log("the value of a + b is ", a+b);
+// console.log("the value of a + b is ", a-b);
+// console.log("the value of a + b is ", a*b);
+// console.log("the value of a + b is ", a/b);
+
+// // assignment operators;
+
+
+// var c = b;
+// c= b;
+// c += 4;
+// console.log(c);
+
+// // 
+// var x = 34;
+// var y = 565;
+// console.log(x == y);
+// console.log(x >= y)
+// console.log(x <= y);
+// console.log(x <y)
+
+
+// // logical operators; and or & not;
+
+// console.log(true|| true);
+// console.log(true|| false);
+// console.log(false|| false);
+
+
+// console.log(!false);
+// console.log(!true)
+
+// // functions  in JS;
+// // dry principle; do not repeat yourself;
+
+
+
+// function avg (a , b){
+
+//     return (a + b)/2;
+
+// }
+
+// c1 = avg(4 , 6)
+// c2 = avg(14, 16 );
+// console.log(c1, c2);
+
+
+// var age = 4;
+// if(age > 8){
+//     console.log("You are not a kid");
+
+// }
+// else{
+//     console.log("You are a kid");
+// }
+
+// // loops;
+// var arr = [1,2, 4, 5, 6, 8, 9 ];
+
+// // console.log(arr);
+// // for(var i = 0; i < arr.length; i++){
+// //     console.log(arr[i]);
+
+// // }
+// // arr.forEach(function(element){
+// //     console.log(element);
+// // })
+
+
+// // While loop;
+
+// let j = 0;
+// const ac= 0;
+
+
+// ac ++ ;
+// // ac = ac+ 1;
+
+// var arr = [1,2, 4, 5, 6, 8, 9 ];
+// while( j < arr.length ){
+//     console.log(arr[j]);
+//     j++;
+
+// }
+
+// var arr = [1,2, 4, 5, 6, 8, 9 ];
+// do{
+//     console.log(arr[j++]);
+    
+//     j++;
+
+// }while(j < arr.length);
+
+// //  arrays;
+
+// let myArr = ["fan", "camera", 34, null, true];
+// //Array methods
+// console.log(myArr.length);
+// // myArr.pop();
+// myArr.push("Harry");
+
+// console.log(myArr);
+//  myArr.toString;
+//  myArr.sort;
+
+
+
+// // console.log(arr);
+
+// for(var i = 0; i < arr.length; i++){
+//     if(i == 2){
+//         // break;
+//         continue;
+//     }
+//     console.log(arr[i]);
+// }
+
+
+var arr = [1, 3, 4, 5, 6, 7, 8, 9];
+let myarr = arr["fan", 34, 56, 7 , true,null, false];
+//Array Methods;
+
+console.log(myarr.length);
+// myarr.pop();
+// 
